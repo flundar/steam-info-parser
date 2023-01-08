7c4cc4677cc806af186808d25b95b72061b20258
flundar <okaysahin__2@hotmail.com>
2023-01-08 16:33:18 +0300

    update

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const axios = require('axios');
 const FormData = require('form-data')
 const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const {
+    JSDOM
+} = jsdom;
 
 
-var bodyFormData = new FormData();
-bodyFormData.append('qsearch', 'flundar');
+
+const form = new FormData();
+form.append('qsearch', "flundar")
 
 axios({
-        method: "post",
-        url: "http://www.vacbanned.com/engine/check",
-        data: bodyFormData,
-        headers: {
-            "Content-Type": "multipart/form-data"
-        },
-    })
-    .then(async function (response) {
+        method: 'POST',
+        url: 'http://www.vacbanned.com/engine/check',
+        data: form,
+        headers: form.getHeaders(),
+    }).then(async function (response) {
         var bilgi = new JSDOM(response.data);
         console.log(bilgi.window.document.querySelector('#main > table:nth-child(3) > tbody > tr:nth-child(4) > td:nth-child(2)').textContent);
     })
     .catch(function (error) {
-        console.log(error);
+        console.log(error)
     });
-
-
-
